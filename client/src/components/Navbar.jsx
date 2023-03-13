4c2f698b9fb424f082ca3f00e34a7a1eab8d97f3
halisa <halisasaipulaa@gmail.com>
2023-03-13 14:51:52 -0700

    rendered the Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+    LightModeOutlined,
+    DarkModeOutlined,
+    Menu as MenuIcon,
+    Search,
+    SettingsOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import { Toolbar,AppBar, useTheme, IconButton ,InputBase,} from "@mui/material";
+
+const Navbar = () => {
+    
+    const dispatch = useDispatch();
+    const theme = useTheme();
+    
+    return (
+        
+        <AppBar
+                sx={{
+                    position: "static",
+                    backgroundColor: "none",
+                    boxShadow: "none",
+                }}
+            >
+                <Toolbar sx={{ justifyContent:"space-between"}}>
+
+                    {/* left side */}
+                    <FlexBetween>
+                        <IconButton onClick={()=> console.log("clicked")}>
+                            <MenuIcon />
+                        </IconButton>
+                        <FlexBetween
+                            backgroundColor={theme.palette.background.alt}
+                            borderRadius="9px"
+                            gap="3rem"
+                            p="0.1rem 1.5rem"
+                        >
+                            <InputBase placeholder="Search..." />
+                            <IconButton>
+                                <Search />
+                            </IconButton>
+
+                        </FlexBetween>
+                    </FlexBetween>
+
+                    {/* right side */}
+                    <FlexBetween gap="1.5rem">
+                        <IconButton onClick={()=> dispatch(setMode())}>
+                            {theme.palette.mode === "dark" ? (
+                                <DarkModeOutlined  sx={{ fontSize:"25px"}} />
+                            ) : (
+                                <LightModeOutlined sx={{ fontSize:"25px"}} />
+                        )} 
+                        </IconButton>
+                        <IconButton>
+                            <SettingsOutlined sx={{ fontSize:"25px"}} />
+                        </IconButton>
+                    </FlexBetween>
+                    
+                </Toolbar>
+
+            </AppBar>
+    )
+}
+
+export default Navbar
--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,10 @@ import globalReducer from "state";
 import { Provider } from 'react-redux';
 
 const store = configureStore({
-    reducer: globalReducer,
-});
+    reducer: {
+        global: globalReducer
+    }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
