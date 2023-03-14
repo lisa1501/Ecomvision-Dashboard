bc4061902def2bf4aa9025ac930a6cc08c948930
halisa <halisasaipullaa@gmail.com>
2023-03-14 14:46:04 -0700

    called api and rendered user on the sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
     ListItem,
     ListItemIcon,
     ListItemText,
+    Divider
 } from "@mui/material";
 import { 
     ChevronLeft,
@@ -23,11 +24,13 @@ import {
     PieChartOutlined,
     AdminPanelSettingsOutlined,
     TrendingUpOutlined,
-    ChevronRightOutlined
+    ChevronRightOutlined,
+    SettingsOutlined
 } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate} from "react-router-dom";
 import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.png";
 
 const navItems = [
     {
@@ -89,6 +92,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+    user,
     drawerWidth,
     isSidebarOpen,
     setIsSidebarOpen,
@@ -185,7 +189,42 @@ const Sidebar = ({
                             })}
                         </List>
                     </Box>
+                    <Box position="absolute" bottom="2rem">
+                        <Divider />
+                        <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+                        <Box
+                            component="img"
+                            alt="profile"
+                            src={profileImage}
+                            height="40px"
+                            width="40px"
+                            borderRadius="50%"
+                            sx={{ objectFit: "cover" }}
+                        />
+                        <Box textAlign="left">
+                            <Typography
+                                fontWeight="bold"
+                                fontSize="0.9rem"
+                                sx={{ color: theme.palette.secondary[100] }}
+                                >
+                                {user.name}
+                                </Typography>
+                                <Typography
+                                fontSize="0.8rem"
+                                sx={{ color: theme.palette.secondary[200] }}
+                                >
+                                {user.occupation}
+                            </Typography>
+                        </Box>
+                        <SettingsOutlined
+                            sx={{
+                            color: theme.palette.secondary[300],
+                            fontSize: "25px ",
+                            }}
+                        />
+                        </FlexBetween>
 
+                    </Box>
                 </Drawer>
             )}
         </Box>
