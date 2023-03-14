a261d868af83e68824849a418cb66b49cfab1d7c
halisa <halisasaipullaa@gmail.com>
2023-03-14 13:48:25 -0700

    created user schema and injected mock data into mongoDb

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -37,4 +41,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // Only add data one time
+    User.insertMany(dataUser);
 }).catch((error) => console.log(`${error} did not connect`));
