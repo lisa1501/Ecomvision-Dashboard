6c7775ba04736f119f703abb26657c0520070be6
halisa <halisasaipulla@gmail.com>
2023-03-15 11:36:42 -0700

    created header component and rendered the Products page

---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from "components/Header";
+
+
+
+const Products = () => {
+    return (
+        <div>
+            <Header title="PRODUCTS" subtitle="See your list of products." />
+        </div>
+    )
+}
+
+export default Products
