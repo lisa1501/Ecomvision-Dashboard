c69214977863d6c0bfd50c7ea53e912e76520a3d
halisa <halisasaipulla@gmail.com>
2023-03-15 14:58:41 -0700

    successfully rendered all products name on the product page

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Box,useMediaQuery} from "@mui/material";
+import { 
+    Box,
+    useMediaQuery,
+    Card,
+    CardContent,
+    Typography,
+} from "@mui/material";
 import Header from "components/Header";
 import { useGetProductsQuery } from 'state/api';
 
+const Product = ({
+    _id,
+    name,
+    description,
+    price,
+    rating,
+    category,
+    supply,
+    productStat,
+    }) => {
+    
+    
+        return (
+        <Card>
+            <CardContent>
+                <Typography>
+                    {name}
+                </Typography>
+            
+            </CardContent>
+            
+        </Card>
+        );
+    };
 const Products = () => {
     const { data, isLoading } = useGetProductsQuery();
     console.log("data", data);
@@ -21,7 +51,32 @@ const Products = () => {
                     sx={{
                         "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
                     }}
-                >           
+                >
+                    {data.map(
+            ({
+                _id,
+                name,
+                description,
+                price,
+                rating,
+                category,
+                supply,
+                productStat,
+                }) => (
+                <Product
+                    key={_id}
+                    _id={_id}
+                    name={name}
+                    description={description}
+                    price={price}
+                    rating={rating}
+                    category={category}
+                    supply={supply}
+                    productStat={productStat}
+                />
+                )
+            )}
+        
                 </Box>
             ):(
                 <>Loading...</>
