bb0c5bc336532101b82a34aea5643badc678aee8
halisa <halisasaipulla@gmail.com>
2023-03-15 11:18:33 -0700

    created products endpoint

---
+++ server/controllers/client.js
@@ -0,0 +1,18 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+
+        const productsWithStats = await Promise.all(
+            products.map(async (product) => {
+                const productStat = await ProductStat.find({productId: product._id});
+                return {...product._doc, productStat };
+            })
+        );
+        res.status(200).json(productsWithStats);
+    } catch (err) {
+        res.status(404).json({message: err.message});
+    }
+}
