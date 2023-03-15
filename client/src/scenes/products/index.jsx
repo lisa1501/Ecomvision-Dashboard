86bfc39b4e014cd760c7cc15eb4645996a12042b
halisa <halisasaipullaa@gmail.com>
2023-03-15 13:42:04 -0700

    fetched data

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
-
+import { Box,useMediaQuery} from "@mui/material";
 import Header from "components/Header";
-
-
+import { useGetProductsQuery } from 'state/api';
 
 const Products = () => {
+    const { data, isLoading } = useGetProductsQuery();
+    console.log("data", data);
+    const isNonMobile = useMediaQuery("(min-width: 1000px)");
     return (
-        <div>
+        <Box m="1.5rem 2.5rem">
             <Header title="PRODUCTS" subtitle="See your list of products." />
-        </div>
+            {data || !isLoading ?(
+                <Box
+                    mt = "20px"
+                    display="grid"
+                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    justifyContent="space-between"
+                    rowGap="20px"
+                    columnGap="1.33%"
+                    sx={{
+                        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                    }}
+                >           
+                </Box>
+            ):(
+                <>Loading...</>
+            )}
+        </Box>
     )
 }
 
