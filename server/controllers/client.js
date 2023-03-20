4045202fd88d9cbebc6ccc2efd58b8275664f2a1
halisa <halisasaipullaa@gmail.com>
2023-03-20 16:16:56 -0700

    implemented server side pagination

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -22,6 +23,45 @@ export const getCustomers= async (req, res) => {
     try {
         const customers = await User.find({role:"user"}).select("-password");
         res.status(200).json(customers);
+    } catch (err) {
+        res.status(404).json({message: err.message});
+    }
+}
+
+export const getTransactions = async (req, res) => {
+    try {
+        // sort should like this:{"field":"userId", "sort":"desc"}
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+        // formatted sort should look like { userId: -1 }
+        const getnerateSort = () =>{
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: sortParsed.sort === "desc"? -1 : 1,
+            };
+            return sortFormatted;
+        }
+
+        const sortFormatted = Boolean(sort)? getnerateSort():{};
+
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i") }},
+                { userId: { $regex: new RegExp(search, "i") }},]
+        })
+
+            .sort(sortFormatted)
+            .skip((page) * pageSize)
+            .limit(pageSize)
+        
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: "i" }
+        })
+
+        res.status(200).json({
+            transactions,
+            total,
+        });
     } catch (err) {
         res.status(404).json({message: err.message});
     }
