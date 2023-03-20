debf7b7eec70cfc13c05f4254e9fbad5dd2aa813
halisa <halisasaipullaa@gmail.com>
2023-03-20 12:10:12 -0700

    styled customers page

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -55,6 +55,30 @@ const Customers = () => {
             <Box
                 mt="40px"
                 height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`,
+                    },
+                }}    
             >
                 <DataGrid
                     loading={isLoading || !data}
