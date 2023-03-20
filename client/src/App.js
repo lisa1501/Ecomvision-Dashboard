8fb365a768fd7caaea946677cc5ca263518439dd
halisa <halisasaipullaa@gmail.com>
2023-03-20 11:17:10 -0700

    called backend Api and rendered the customers page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { themeSettings } from "theme";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Dashboard from "scenes/dashboard";
 import Products from "scenes/products";
+import Customers from "scenes/customers";
 import Layout from "scenes/layout";
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
                             <Route path="/" element={<Navigate to="/dashboard" replace />} />
                             <Route path="/dashboard" element={<Dashboard />} />
                             <Route path="/products" element={<Products />} />
+                            <Route path="/customers" element={<Customers />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>
