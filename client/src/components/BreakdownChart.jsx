b56cccd39e4e763a55ad2d0ca24f8e594f7ed360
halisa <halisasaipulla@gmail.com>
2023-03-23 10:42:38 -0700

    rendered a piechart

--- client/src/components/BreakdownChart.jsx
+++ client/src/components/BreakdownChart.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import { useGetSalesQuery } from "state/api";
+import { Box, useTheme } from "@mui/material";
 
-const BreakdownChart = () => {
+import { ResponsivePie } from "@nivo/pie";
+
+const BreakdownChart = ({ isDashboard = false }) => {
+    const theme = useTheme();
+
     const { data, isLoading } = useGetSalesQuery();
     if (!data || isLoading) return "Loading...";
     console.log(data)
+
+    const colors = [
+        theme.palette.secondary[500],
+        theme.palette.secondary[300],
+        theme.palette.secondary[300],
+        theme.palette.secondary[500],
+    ];
+
+    const formattedData = Object.entries(data.salesByCategory).map(
+        ([category, sales], i) => ({
+            id: category,
+            label: category,
+            value: sales,
+            color: colors[i],
+        })
+    );
+    
     
     return (
-        <div>Breakdown chart</div>
+        <Box
+            height={isDashboard ? "400px" : "100%"}
+            width={undefined}
+        >
+            <ResponsivePie 
+                data={formattedData}            
+            />   
+        </Box>    
     );
 };
 
