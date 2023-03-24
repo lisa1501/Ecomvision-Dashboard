ae2141e8eab35949ee19d5bb2b7e7c2b71412a1a
halisa <halisasaipullaa@gmail.com>
2023-03-24 11:06:10 -0700

    created performance schema

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
 import OverallStat from "./models/OverallStat.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js";
+import AffiliateStat from "./models/AffilliateStat.js";
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -52,4 +53,5 @@ mongoose.connect(process.env.MONGO_URL, {
     // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat);
 }).catch((error) => console.log(`${error} did not connect`));
