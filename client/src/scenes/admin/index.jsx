6e1dca534ff90e41de65b314a67d25862fef0b12
halisa <halisasaipulla@gmail.com>
2023-03-24 10:36:56 -0700

    rendered data and styled the admin page

--- client/src/scenes/admin/index.jsx
+++ client/src/scenes/admin/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useGetAdminsQuery } from "state/api";
-import { Box } from "@mui/material";
+import { Box,useTheme } from "@mui/material";
 import Header from "components/Header";
 import { DataGrid } from "@mui/x-data-grid";
 
 const Admin = () => {
-
+    const theme = useTheme();
     const { data, isLoading } = useGetAdminsQuery();
     console.log("data", data);
 
@@ -53,7 +53,33 @@ const Admin = () => {
     return (
         <Box m="1.5rem 2.5rem">
             <Header title="ADMINS" subtitle="Managing admins and list of admins" />
-            <Box>
+            <Box
+                mt="40px"
+                height="75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none",
+                    },
+                    "& .MuiDataGrid-cell": {
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-columnHeaders": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderBottom: "none",
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: theme.palette.primary.light,
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        backgroundColor: theme.palette.background.alt,
+                        color: theme.palette.secondary[100],
+                        borderTop: "none",
+                    },
+                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                        color: `${theme.palette.secondary[200]} !important`,
+                    },
+                }}>
                 <DataGrid
                     loading={isLoading || !data}
                     getRowId={(row) => row._id}
