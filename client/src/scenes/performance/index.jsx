49609cbc16c9cdd9a95e42d98fca978452cb92fd
halisa <halisasaipulla@gmail.com>
2023-03-24 13:14:09 -0700

    successsfully fetched valid user data

--- client/src/scenes/performance/index.jsx
+++ client/src/scenes/performance/index.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useTheme } from "@mui/material";
+import { useGetUserPerformanceQuery } from "state/api";
+import { useSelector } from "react-redux";
+
 
 const Performance = () => {
+    const userId = useSelector((state) => state.global.userId);
+    // console.log(userId);
+    const { data, isLoading } = useGetUserPerformanceQuery(userId);
+    console.log("data", data);
+
     return (
         <div>
         Performance
