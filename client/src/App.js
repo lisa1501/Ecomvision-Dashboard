3bff231a2094484cd9821b3eb06cf599269dba28
halisa <halisasaipulla@gmail.com>
2023-03-24 09:15:21 -0700

    added admin path and scene

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
 import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+
 function App() {
     const mode = useSelector((state) => state.global.mode);
     // console.log(mode);
@@ -36,6 +38,7 @@ function App() {
                             <Route path="/daily" element={<Daily />} />
                             <Route path="/monthly" element={<Monthly />} />
                             <Route path="/breakdown" element={<Breakdown />} />
+                            <Route path="/admin" element={<Admin />} />
                         </Route>
                     </Routes>
                 </ThemeProvider>
