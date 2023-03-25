8a8487542e2b40bbe87ad269ced5fd253de0b1f6
halisa <halisasaipulla@gmail.com>
2023-03-25 08:03:50 -0700

    successfully fetched dashboard data from backend

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useGetDashboardQuery } from "state/api";
 
 function Dashboard() {
+    const { data, isLoading } = useGetDashboardQuery();
+    console.log("data", data);
     return (
         <div>
             Dashboard
