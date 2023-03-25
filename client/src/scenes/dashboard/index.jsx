08c4c9bf65f2aac358a47c5287170448bc780cd6
halisa <halisasaipulla@gmail.com>
2023-03-25 08:47:48 -0700

    added download button

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,13 +1,36 @@
+import FlexBetween from 'components/FlexBetween';
 import React from 'react';
 import { useGetDashboardQuery } from "state/api";
+import { Box, Button,useTheme, } from "@mui/material"
+import Header from "components/Header";
+import { DownloadOutlined } from "@mui/icons-material";
+
 
 function Dashboard() {
+    const theme = useTheme();
     const { data, isLoading } = useGetDashboardQuery();
     console.log("data", data);
+        
     return (
-        <div>
-            Dashboard
-        </div>
+        <Box m="1.5rem 2.5rem">
+            <FlexBetween>
+                <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+                <Box>
+                    <Button
+                        sx={{
+                        backgroundColor: theme.palette.secondary.light,
+                        color: theme.palette.background.alt,
+                        fontSize: "14px",
+                        fontWeight: "bold",
+                        padding: "10px 20px",
+                        }}
+                    >
+                        <DownloadOutlined sx={{ mr: "10px" }} />
+                            Download Reports
+                    </Button>
+                </Box>
+            </FlexBetween>
+        </Box>
     )
 }
 
