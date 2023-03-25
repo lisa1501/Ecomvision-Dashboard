8ddccb674e1b169aaa09c85549dcaa8fc993bc84
halisa <halisasaipulla@gmail.com>
2023-03-25 08:56:46 -0700

    created stat box component and rendered total customers box on the dashboard page

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { useGetDashboardQuery } from "state/api";
 import { Box, Button,useTheme, } from "@mui/material"
 import Header from "components/Header";
-import { DownloadOutlined } from "@mui/icons-material";
+import { DownloadOutlined,Email } from "@mui/icons-material";
+import StatBox from "components/StatBox";
 
 
 function Dashboard() {
@@ -18,11 +19,11 @@ function Dashboard() {
                 <Box>
                     <Button
                         sx={{
-                        backgroundColor: theme.palette.secondary.light,
-                        color: theme.palette.background.alt,
-                        fontSize: "14px",
-                        fontWeight: "bold",
-                        padding: "10px 20px",
+                            backgroundColor: theme.palette.secondary.light,
+                            color: theme.palette.background.alt,
+                            fontSize: "14px",
+                            fontWeight: "bold",
+                            padding: "10px 20px",
                         }}
                     >
                         <DownloadOutlined sx={{ mr: "10px" }} />
@@ -30,6 +31,26 @@ function Dashboard() {
                     </Button>
                 </Box>
             </FlexBetween>
+            <Box
+                mt="20px"
+                display="grid"
+                gridTemplateColumns="repeat(12, 1fr)"
+                gridAutoRows="160px"
+                gap="20px"
+            >
+                <StatBox
+                    title="Total Customers"
+                    value={data && data.totalCustomers}
+                    increase="+14%"
+                    description="Since last month"
+                    icon={
+                        <Email
+                        sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+                        />
+                    }
+                />
+
+            </Box>
         </Box>
     )
 }
