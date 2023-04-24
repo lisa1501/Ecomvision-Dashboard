11cb93343760452dc2c77b0592e1dfe49570d947
Lisa S <halisasaipulla@gmail.com>
2023-04-24 10:05:46 -0700

    added images folder

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { getUser, getDashboardStats } from '../controllers/general.js';
+import  { logout, postLogin, test }  from "../controllers/auth.js";
+import ensureAuth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get('/user/:id', getUser);
-router.get('/dashboard', getDashboardStats);
+router.get('/dashboard',  ensureAuth,getDashboardStats);
+
+router.post("/login", postLogin);
+router.get("/test", test);
+router.get("/logout", logout);
 
 export default router;
